a3001dbb2c094d8567493a539506627b87992497
Technovishal07 <vishalkumar06762@gmail.com>
2026-03-25 14:03:02 +0530

    Added matrimony route with JWT

--- Backend/src/services/matrimony.service.ts
+++ Backend/src/services/matrimony.service.ts
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// export const fetchMatrimonyData = async () => {
-//   try {
-//     const response = await axios.get(
-//       "http://127.0.0.1:8000/matrimony-data"
-//     );
-
-//     return response.data;
-
-//   } catch (error: any) {
-//     throw new Error("Service Error: " + error.message);
-//   }
-// };
-
-
 import axios from "axios";
 
 export const createProfileService = async (data: any) => {
